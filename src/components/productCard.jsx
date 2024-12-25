da62160ea70d5d89231cbe8b302f5102e67e566c
Kavindu-Prashan <kavinduprashan7@gmail.com>
2024-12-25 19:39:47 +0530

    initilized frontend

---
+++ src/components/productCard.jsx
@@ -0,0 +1,9 @@
+export function ProductCard() {
+    return(
+        <div>
+            <h1>Product One</h1>
+            <h2>Price: $100</h2>
+            <button>Add to cart</button>
+        </div>
+    )
+}

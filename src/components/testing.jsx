95a41497d7a940e591246dc00c055b8bfdfad8fe
Kavindu-Prashan <kavinduprashan7@gmail.com>
2024-12-26 17:40:46 +0530

    created hooks for the testing component (useState)

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -1,19 +1,26 @@
+import { useState } from "react"
 import "./testing.css"
 
 export default function Testing() {
 
-    let count = 0
+    const [count,setCount] = useState(0)
 
     function increment() {
         console.log("incrementing")
+        setCount(count+1) 
+    }
+
+    function decrement() {
+        console.log("Decrementing")
+        setCount(count-1)  
     }
 
 
 
     return(
         <div className="background">
-            <button >-</button>
-            <span>0</span>
+            <button onClick={decrement} >-</button>
+            <span>{count}</span>
             <button onClick={increment}>+</button>
         </div>
     )

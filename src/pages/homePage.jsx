25a50d28d0c755d263a1728b205a6fe6c8e2c1a7
Kavindu-Prashan <kavinduprashan7@gmail.com>
2025-01-09 20:13:32 +0530

    installed tailwind css

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,25 +1,9 @@
-import "./homePage.css"
-import {Link} from 'react-router-dom'
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-export default function HomePage() {
+
+export default function homePage() {
   return (
-    <div className="homepage">
-      <nav className="navbar">
-        <div className="logo">MyWebsite</div>
-        <ul className="nav-links">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#services">Services</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
-
-      <header className="hero">
-        <h1>Welcome to MyWebsite</h1>
-        <p>Your one-stop solution for everything!</p>
-        <button>Get Started</button>
-        <Link to="/login">Login</Link>
-      </header>
-    </div>
-  );
+    <div><h1 className='text-red-950'>HomePage</h1></div>
+  )
 }
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,17 +1,12 @@
-
-import "./loginPage.css"
-import {Link} from 'react-router-dom'
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
-    return(
-        <div>
-            <h1>Login Page</h1>
-            <input type="text" placeholder="Enter Your Name"/>
-            <input type="Password" placeholder="Enter Your Password" />
-            <button>Login</button>
-            <Link to="/">Home</Link>
-
-        </div>
-    )
+  return (
+    <div>
+      <button className="bg-yellow-500 text-white px-4 py-2 rounded">
+        Login
+      </button>
+    </div>
+  );
 }

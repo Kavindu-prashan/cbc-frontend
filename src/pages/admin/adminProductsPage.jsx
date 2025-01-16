4caff670f2c0db8477b80337abaae3de8f1a2335
Kavindu-Prashan <kavinduprashan7@gmail.com>
2025-01-16 10:49:33 +0530

    create link with front and and backend

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,6 +1,9 @@
+import axios from "axios"
 export default function AdminProductsPage(){
 
-    
+    axios.get("http://localhost:5000/api/products").then((res)=>{
+        console.log(res)
+    })
 
     return(
         <div>Admin product Page</div>

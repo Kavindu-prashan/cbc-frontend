65d51702e96158eb62dfddefe5c6bd99976e5454
Kavindu-Prashan <kavinduprashan7@gmail.com>
2025-01-17 22:42:50 +0530

    add onchange for the login email and password

--- src/pages/admin/adminProductsPage.jsx
+++ src/pages/admin/adminProductsPage.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 export default function AdminProductsPage(){
 
-    axios.get("http://localhost:5000/api/products").then((res)=>{
+    axios.get("http://localhost:5000/api/products/").then((res)=>{
         console.log(res)
     })
 
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,36 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
+
+  const [email,setEmail] = useState("Your email")
+  const [password,setpassword] = useState("")
+  
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded shadow-md">
-        <h1 className="text-2xl font-bold text-center mb-6">Login Page</h1>
-        <div className="mb-4">
-          <input
-            type="text"
-            placeholder="Enter your username"
-            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-300"
-          />
-        </div>
-        <div className="mb-6">
-          <input
-            type="password"
-            placeholder="Enter your password"
-            className="w-full px-4 py-2 border rounded focus:outline-none focus:ring focus:ring-blue-300"
-          />
-        </div>
-        <button className="w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
-          Login
-        </button>
-        <div className="text-center mt-4">
-          <Link
-            to="/"
-            className="text-blue-500 hover:underline focus:ring focus:ring-blue-300"
-          >
-            Home
-          </Link>
-        </div>
+    <div className='flex justify-center items-center w-full h-screen bg-slate-400' >
+      <div className='flex flex-col justify-center items-center w-[450px] h-[400px] bg-slate-500' >
+        <img src='/logo.webp' className='rounded-full w-[100px]'></img>
+
+        <span>Email</span>
+        <input type="text" defaultValue={email} onChange={
+          (e)=>{
+            setEmail(e.target.value)
+          }
+        }/>
+
+        <span>Password</span>
+        <input type="password" defaultValue={password} onChange={
+          (e)=>{
+            setpassword(e.target.value)
+          }
+        }/>
+        <button className='bg-slate-400 rounded'>Login</button>
       </div>
     </div>
   );
 }
+     
